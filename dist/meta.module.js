dda9885844ead6dccf121baa0b3deed8a04e71ac
Angel M De Miguel <angel@bitnami.com>
2017-02-17 16:32:40 +0100

    Compile the project

---
+++ dist/meta.module.js
@@ -0,0 +1,33 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { OpaqueToken, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MetaService } from './meta.service';
+export var META_CONFIG = new OpaqueToken('meta config');
+var MetaModule = MetaModule_1 = (function () {
+    function MetaModule() {
+    }
+    MetaModule.forRoot = function (metaConfig) {
+        if (metaConfig === void 0) { metaConfig = { useTitleSuffix: false, defaults: {} }; }
+        return {
+            ngModule: MetaModule_1,
+            providers: [
+                { provide: META_CONFIG, useValue: metaConfig },
+                MetaService
+            ]
+        };
+    };
+    return MetaModule;
+}());
+MetaModule = MetaModule_1 = __decorate([
+    NgModule({
+        imports: [RouterModule]
+    })
+], MetaModule);
+export { MetaModule };
+var MetaModule_1;
+//# sourceMappingURL=meta.module.js.map
